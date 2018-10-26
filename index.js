52406d9ad8a99b757210d661fa3cb3d8c60304fc
Shin-JaeHeon <heoni0504@gmail.com>
2018-10-26 20:54:13 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import Map from './src/Map';
+
+const vueNaverMaps = {
+  /**
+   *
+   * @param {Vue} Vue
+   * @param {
+   * {
+   * clientID:string
+   * subModules?:string
+   * }
+   * }options
+   */
+  install(Vue, options) {
+    if (options.clientID) {
+      const URL = `https://openapi.map.naver.com/openapi/v3/maps.js?clientId=${options.clientID}${options.subModules ? `&submodules=${options.subModules}` : ''}`;
+      const SCRIPT = document.createElement('script');
+      if (SCRIPT)
+        SCRIPT.setAttribute('src', URL);
+      SCRIPT.id = 'naver-map-load';
+      SCRIPT.setAttribute('async', '');
+      SCRIPT.setAttribute('defer', '');
+      SCRIPT.onload = () => {
+        Vue.isNaverMapLoaded = true;
+      };
+      document.head.appendChild(SCRIPT);
+      Vue.isNaverMapLoaded = false;
+    } else throw new Error('options must includes clientID');
+    Vue.component('NaverMaps', Map);
+
+  }
+};
+
+export default vueNaverMaps;
---
+++ src/Map.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="vue-naver-maps" :style="mapStyle"></div>
+</template>
+
+<script>
+  export default {
+    name: 'Map',
+    props: {
+      width: Number,
+      height: Number,
+      mapOptions: {
+        zoom: Number,
+        lat: Number,
+        lng: Number
+      }
+    },
+    data() {
+      return {
+        mapStyle: {
+          width: `${this.width}px`,
+          height: `${this.height}px`,
+        }
+      }
+    },
+
+    mounted() {
+      document.getElementById('naver-map-load').onload = () => {
+        var map = new naver.maps.Map('vue-naver-maps', {
+          center: new naver.maps.LatLng(this.mapOptions.lat, this.mapOptions.lng),
+          zoom: this.mapOptions.zoom
+        });
+      };
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
