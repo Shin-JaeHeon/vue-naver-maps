80c6cd65e0170848cd2af29a3e6c008fba087e37
Shin-JaeHeon <heoni0504@gmail.com>
2018-11-24 15:54:56 +0900

    add NaverEllipse Component

---
+++ src/overlays/NaverEllipse.vue
@@ -0,0 +1,187 @@
+<template>
+</template>
+
+<script>
+  export default {
+    name: 'NaverEllipse',
+    props: {
+      bounds: Object,
+      moreOptions: {
+        type: Object,
+        strokeWeight: Number,
+        strokeOpacity: Number,
+        strokeColor: String,
+        strokeStyle: Object,
+        strokeLineCap: Object,
+        strokeLineJoin: Object,
+        fillColor: String,
+        fillOpacity: Number,
+        clickable: Boolean,
+        visible: Boolean,
+        zIndex: Number
+      },
+    },
+    data() {
+      return {
+        /**
+         * {naver.maps.Ellipse} ellipse
+         */
+        ellipse: null,
+        /**
+         * {naver.maps.Map} map
+         */
+        map: null,
+      };
+    },
+    methods: {
+      /**
+       *
+       * @returns {number}
+       */
+      getAreaSize() {
+        return this.ellipse.getAreaSize();
+      },
+
+      /**
+       *
+       * @returns {naver.maps.Bounds}
+       */
+      getBounds() {
+        return this.ellipse.getBounds();
+      },
+      /**
+       *
+       * @returns {boolean}
+       */
+      getClickable() {
+        return this.ellipse.getClickable();
+      },
+
+      /**
+       *
+       * @returns {naver.maps.Bounds}
+       */
+      getDrawingRect() {
+        return this.ellipse.getDrawingRect();
+      },
+
+      /**
+       *
+       * @param key
+       * @returns {naver.maps.CircleOptions}
+       */
+      getOptions(key) {
+        return this.ellipse.getOptions(key);
+      },
+      /**
+       *
+       * @param key
+       * @returns {naver.maps.CircleOptions}
+       */
+      getStyles(key) {
+        return this.ellipse.getStyles(key);
+      },
+
+      /**
+       *
+       * @returns {boolean}
+       */
+      getVisible() {
+        return this.ellipse.getVisible();
+      },
+
+      /**
+       *
+       * @returns {number}
+       */
+      getZIndex() {
+        return this.ellipse.getZIndex();
+      },
+      /**
+       * @param bounds {naver.maps.Bounds}
+       * @returns {this}
+       */
+      setBounds(bounds) {
+        this.ellipse.setBounds(bounds);
+        return this;
+      },
+
+      /**
+       *
+       * @param clickable
+       * @returns {this}
+       */
+      setClickable(clickable) {
+        this.ellipse.setClickable(clickable);
+        return this;
+      },
+
+      /**
+       *
+       * @param options
+       * @returns {this}
+       */
+      setOptions(options) {
+        if (arguments[1]) this.ellipse.setOptions(options, arguments[1]);
+        else this.ellipse.setOptions(options);
+        return this;
+      },
+
+      /**
+       *
+       * @param radius
+       * @returns {this}
+       */
+      setRadius(radius) {
+        this.ellipse.setRadius(radius);
+        return this;
+      },
+
+      /**
+       *
+       * @param options
+       * @returns {this}
+       */
+      setStyles(options) {
+        if (arguments[1]) this.ellipse.setStyles(options, arguments[1]);
+        else this.ellipse.setStyles(options);
+        return this;
+      },
+
+      /**
+       *
+       * @param visible
+       * @returns {this}
+       */
+      setVisible(visible) {
+        this.ellipse.setVisible(visible);
+        return this;
+      },
+
+      /**
+       *
+       * @param zIndex
+       * @returns {this}
+       */
+      setZIndex(zIndex) {
+        this.ellipse.setZIndex(zIndex);
+        return this;
+      },
+
+    },
+    mounted() {
+      window.$naverMapsCallback.push((map) => {
+        /**
+         * {naver.maps.Map} map
+         */
+        this.map = map;
+        this.ellipse = new naver.maps.Ellipse(Object.assign({bounds: this.bounds}, this.moreOptions));
+        this.$emit('load', this);
+      });
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
