95de87691f7ea1fe90587b1387bcc8d7940a04a1
Bruce Shin <ssshow16@gmail.com>
2023-04-25 08:35:27 +0900

    browsers 지원 로직 오류 수정

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const vueNaverMaps = {
    * } options
    */
   install(Vue, options) {
-    if (typeof window === "undefined") {
+    if (typeof window !== "undefined") {
       if (options.clientID) {
         window.$naverMapsCallback = [];
         window.$naverMapsLoaded = false;
